192f67f740ff1acde2bf3d90ae954a670f59e0ea
Martin Klimovski <martin_klimovski@live.com>
2024-11-01 19:14:21 +0100

    'Hello'

--- tests/magento-ecom/fixtures/fixtures.ts
+++ tests/magento-ecom/fixtures/fixtures.ts
@@ -1,10 +1,12 @@
 import {test as base, Page} from '@playwright/test'
 import { CreateAnAccountPage } from '../pages/createAnAccountPage'
 import { ProductPage } from '../pages/productPage';
+import { FirstPage } from '../pages/firstPage';
 export {expect} from '@playwright/test';
 type Fixtures = {
     createAnAccountPage: CreateAnAccountPage;
     productPage: ProductPage;
+    firstPage: FirstPage;
 };
 export const test= base.extend<Fixtures>({
     createAnAccountPage: async ({page}, use) =>{
@@ -14,5 +16,8 @@ export const test= base.extend<Fixtures>({
     productPage: async ({page}, use) =>{
         const productPage = new ProductPage(page);
         await use(productPage);
+    },
+    firstPage: async ({page}, use) =>{
+        await use (new FirstPage(page));
     }
 })
